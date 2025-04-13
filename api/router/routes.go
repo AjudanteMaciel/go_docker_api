2de9d7aa1d094e0473ec307e1493753c9306a706
Maciel <macieldfaria@gmail.com>
2025-04-13 02:44:28 -0300

    removing handlerutil

--- api/router/routes.go
+++ api/router/routes.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"go_docker_api/api/handler/employee"
-	"go_docker_api/api/handler/handlerutil"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,13 +16,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.POST("/employee", employee.CreateEmployeeHandler)
 		v1.PUT("/employee/:id", employee.UpdateEmployeeHandler)
 		v1.DELETE("/employee/:id", employee.DeleteEmployeeHandler)
-	}
-	{
-		v1.GET("/ping", handlerutil.PingHandler)
-	}
-	// I KNOW THIS IS DUMB BUT I AM JUST TESTING THE DROP DATABASE FUNCTIONALITY
-	{
-		v1.GET("/drop_database", handlerutil.DropDatabaseHandler)
 	}
 
 }
